132377eb3f1d218598b2b07876949772e459a6a5
raja <rajababu095221@gmail.com>
2024-02-03 17:53:51 +0000

    changed name and title

--- src/components/ProfileSection/index.js
+++ src/components/ProfileSection/index.js
@@ -49,13 +49,13 @@ class ProfileSection extends Component {
 
   renderProfileSuccess = () => {
     const {profileDetails} = this.state
-    const {name, profileImageUrl, shortBio} = profileDetails
+    const {profileImageUrl} = profileDetails
 
     return (
       <div className="profile-section-container">
         <img src={profileImageUrl} alt="profile" className="profile-img" />
-        <h1 className="profile-name">{name}</h1>
-        <p className="profile-bio">{shortBio}</p>
+        <h1 className="profile-name">Venkat</h1>
+        <p className="profile-bio">Aspiring Full-Stack Developer</p>
       </div>
     )
   }
